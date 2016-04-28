5f5744ea14ee5cabf256cb0fe3b886828b242c4d
Justin Hester <justinlawrencehester@gmail.com>
2016-04-28 15:15:07 -0700

    Solved Day 2 Exercise 2

---
+++ src/com/day02/Exercise02.java
@@ -0,0 +1,13 @@
+package com.day02;
+
+public class Exercise02 {
+    public static void main(String[] arguments) {
+        int n1 = 5;
+        int n2 = -3;
+        int result, remainder;
+        result = n1 / n2;
+        remainder = n1 % n2;
+        System.out.println(result + "\t" + remainder);
+
+    }
+}
