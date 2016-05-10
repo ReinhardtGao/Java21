868372628349d865c36de972d55d85b99b64752d
Justin Hester <justinlawrencehester@gmail.com>
2016-05-10 10:30:42 -0700

    Included override method in SubPrinter for printMe.

--- src/com/java21days/Printer.java
+++ src/com/java21days/Printer.java
@@ -14,6 +14,13 @@ class Printer {
 class SubPrinter extends Printer {
     int z = 3;
 
+    @Override
+    void printMe() {
+        System.out.println("x is " + x + ", y is " + y + ", z is " + z);
+        System.out.println("I am an instance of the class " +
+            this.getClass().getName());
+    }
+
     public static void main(String[] arguments) {
         SubPrinter obj = new SubPrinter();
         obj.printMe();
