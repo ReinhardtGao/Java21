01c5f91a5a450577fad56c3d3c034c7fc229ee62
Justin Hester <justinlawrencehester@gmail.com>
2016-06-06 13:26:19 -0700

    Solved Day 10 Exercise 2.

---
+++ src/com/java21days/ProgressMonitor2.java
@@ -0,0 +1,55 @@
+package com.java21days;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class ProgressMonitor2 extends JFrame {
+
+    JProgressBar current;
+    JTextField field = new JTextField(4);
+    JTextArea out;
+    JButton find;
+    int num = 0;
+
+    public ProgressMonitor2() {
+        super("Progress Monitor 2");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLookAndFeel();
+        setSize(480, 60);
+        setLayout(new FlowLayout());
+        current = new JProgressBar(0, 2000);
+        current.setValue(0);
+        current.setStringPainted(true);
+        add(field);
+        add(current);
+    }
+
+    public void iterate() {
+        while (num < 2000) {
+            current.setValue(num);
+            field.setText(Integer.toString(num));
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) { }
+            num += 95;
+        }
+    }
+
+    private void setLookAndFeel() {
+        try {
+            UIManager.setLookAndFeel(
+                "com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel"
+            );
+            SwingUtilities.updateComponentTreeUI(this);
+        } catch (Exception e) {
+            System.err.println("Couldn't use the system "
+                + "look and feel: " + e);
+        }
+    }
+
+    public static void main(String[] arguments) {
+        ProgressMonitor2 frame = new ProgressMonitor2();
+        frame.setVisible(true);
+        frame.iterate();
+   }
+}
