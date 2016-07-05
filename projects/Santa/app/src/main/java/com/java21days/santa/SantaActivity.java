b6247a25c837520f56969097b051b23f31c1f9c2
Justin Hester <justinlawrencehester@gmail.com>
2016-07-05 13:06:02 -0700

    Solved Day 21 Exercise 2.

--- projects/Santa/app/src/main/java/com/java21days/santa/SantaActivity.java
+++ projects/Santa/app/src/main/java/com/java21days/santa/SantaActivity.java
@@ -29,15 +29,15 @@ public class SantaActivity extends AppCompatActivity {
         switch (id) {
             case (R.id.imageButton):
                 action = new Intent(Intent.ACTION_DIAL,
-                        Uri.parse("tel:877-446-6723"));
+                        Uri.parse("tel:212-998-3282"));
                 break;
             case (R.id.imageButton2):
                 action = new Intent(Intent.ACTION_VIEW,
-                        Uri.parse("http://www.noradsanta.org"));
+                        Uri.parse("http://yann.lecun.com/"));
                 break;
             case (R.id.imageButton3):
                 action = new Intent(Intent.ACTION_VIEW,
-                        Uri.parse("geo:0,0?q=101 Saint Nicholas Dr., North Pole, AK"));
+                        Uri.parse("geo:715 Broadway, New York, NY 10012, USA"));
                 break;
             default:
                 break;
